7de6e2008a35a66ed27a20379537f8542a186878
superrabbit11223344 <gongwei808@hotmail.com>
2019-07-26 14:24:38 +0800

    haaha

--- blog/models.py
+++ blog/models.py
@@ -22,12 +22,19 @@ class Post(models.Model):
         verbose_name = verbose_name_plural = "文章"
 
 
-
-
-
-
-
-
-
 class Category(models.Model):
-    pass
+    STATUS_ITEMS = (
+        (1, '可用'),
+        (2, '删除')
+    )
+    name = models.CharField(max_length=50, verbose_name="名称")
+    status = models.PositiveIntegerField(default=1, choices=STATUS_ITEMS, verbose_name="状态")
+    is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
+
+    owner = models.ForeignKey(User, verbose_name="作者")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "分类"
+
+class Tag(models.Model):
