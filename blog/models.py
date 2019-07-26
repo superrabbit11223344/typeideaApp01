d0b71c76488323a7392004ea26fbd80fd816473e
superrabbit11223344 <gongwei808@hotmail.com>
2019-07-26 14:33:46 +0800

    haaha

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 
     content = models.TextField(verbose_name="内容", help_text="注：目前只支持markdown数据")
     status = models.IntegerField(default=1, choices=,verbose_name="状态")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=True)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
@@ -31,10 +31,23 @@ class Category(models.Model):
     status = models.PositiveIntegerField(default=1, choices=STATUS_ITEMS, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
 
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=True)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = "分类"
 
 class Tag(models.Model):
+    STATUS_ITEMS = (
+        (1, '正常'),
+        (2, '删除'),
+    )
+
+    name = models.CharField(max_length=10, verbose_name="名称")
+    status = models.PositiveIntegerField(default=1, choices=STATUS_ITEMS, verbose_name="状态")
+
+    owner = models.ForeignKey(User, verbose_name="作者")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = '标签'
