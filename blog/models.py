70bce0d77f7f373987722c77d661baeaad975821
superrabbit11223344 <gongwei808@hotmail.com>
2019-07-26 14:01:15 +0800

    haaha

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, verbose_name="标题")
+    desc = models.CharField(max_length=255, blank=True, verbose_name="摘要")
+    category = models.ForeignKey(Category, verbose_name="分类")
+
+
+
+
+class Category(models.Model):
+    pass
