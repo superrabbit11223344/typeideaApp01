f2ca13bfdbe4c360dee4913f42a3c15fcb8a7bee
superrabbit11223344 <gongwei808@hotmail.com>
2019-07-27 16:16:55 +0800

    haaha

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import connection
 from django.test import TestCase
 
-# Create your tests here.
+
+from .models import Category
+
+class TestCategory(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('the5fire', 'fake@email.com', 'password')
+        for i in range(10):
+            category_name = 'cate_%s' % i
+            Category.objects.create(name=category_name, owner=user)
+
+
+
+    def test_filter(self):
+        queryset = Category.objects.all()
+        print(type(queryset))
+        categories = queryset.filter(status=1)
+        print(type(categories))
+        categories = queryset.exclude(status=0)
+        print(type(categories))
