e94ebac7d8c0b91cdd5142341c5a6243d33f6414
superrabbit11223344 <gongwei808@hotmail.com>
2019-07-30 10:38:14 +0800

    haaha

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 
 from .models import Post, Category, Tag
+from typeideaApp.custom_site import custom_site
 
-
-@admin.register(Category)
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Post)  # site自定义
+@admin.register(Post, site=custom_site)  # site自定义
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'category', 'status', 'owner', 'created_time']
+    search_fields = ['title', 'category__name', 'owner__username']
+    list_filter = ['category', 'owner']
+    save_on_top = True
