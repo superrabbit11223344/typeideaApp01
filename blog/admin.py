79eee86902f3d84d5aa1506247c48ab176e3b763
superrabbit11223344 <gongwei808@hotmail.com>
2019-07-31 12:46:46 +0800

    haaha

--- blog/admin.py
+++ blog/admin.py
@@ -4,19 +4,12 @@ from django.urls import reverse
 
 from .models import Post, Category, Tag
 from typeideaApp.custom_site import custom_site
-
-@admin.register(Category, site=custom_site)
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Tag, site=custom_site)
-class TagAdmin(admin.ModelAdmin):
-    pass
-
+from typeideaApp.custom_admin import BaseOwnerAdmin
+from .adminforms import PostAdminForm
 
 @admin.register(Post, site=custom_site)  # site自定义
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
     list_display = [
         'title', 'category',
         'status', 'status_show',
@@ -24,23 +17,37 @@ class PostAdmin(admin.ModelAdmin):
         'operator'
                     ]
     # list_display_links = ['title', 'category']
+    list_display_links = []
 
-    search_fields = ['title', 'category__name', 'owner__username']
     list_filter = ['category', 'owner']
-    save_on_top = False
+    search_fields = ['title', 'category__name']
+    save_on_top = True
+    actions_on_top = True
 
     date_hierarchy = 'created_time'
     # list_editable = ('title','status')
     # fields = (('category', 'title'), 'content')  # 布局
 
-    fieldsets = (  # 跟fields互斥
-        ('基础配置', {
-            'fields': (('category', 'title'), 'content')
-        }),
-        ('高级配置', {
-            'fields': ('tag',),
-        }),
-    )
+    # fieldsets = (  # 跟fields互斥
+    #     ('基础配置', {
+    #         'fields': (
+    #             ('category', 'title'),
+    #             'desc',
+    #             'status',
+    #             'content'
+    #         )
+    #     }),
+    #     ('高级配置', {
+    #         'fields': ('tag',),
+    #     }),
+    # )
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+         'content',
+        'tag',
+            )
 
     def operator(self, obj):
         return format_html(
@@ -48,5 +55,26 @@ class PostAdmin(admin.ModelAdmin):
             reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = '操作'
+
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    list_display = ['name', 'status', 'is_nav', 'created_time']
+    fields = (
+        'name', 'status',
+        'is_nav',
+    )
+
+
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ['name', 'status', 'created_time']
+    fields = (
+        'name', 'status'
+    )
+
+
+
+
 
 
