b2cdb7fdf59270a07872202809bd68b63cf9d5ec
superrabbit11223344 <gongwei808@hotmail.com>
2019-08-02 15:16:35 +0800

    haaha

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from .models import Post
+from django.http import Http404
+
+from .models import Post, Tag
 
 
 def post_list(request, category_id=None, tag_id=None):
     queryset = Post.objects.all()
     if category_id:
-        qeuryset = queryset.objects.filter(category_id=category_id)
+        # 分类页面
+        qeuryset = queryset.filter(category_id=category_id)
     elif tag_id:
-        queryset = queryset.objects.filter(tag_id=tag_id)
+        # 标签页面
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            queryset = []
+        else:
+            queryset = tag.post_set.all()
     context = {
         'posts': queryset
     }
@@ -15,7 +24,11 @@ def post_list(request, category_id=None, tag_id=None):
 
 
 def post_detail(request, post_id=None):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        raise Http404('出错啦！！文章找不到')
     context = {
-        'name': 'post_detail'
+        'post': post
     }
     return render(request, 'blog/detail.html', context=context)
--- typeideaApp/urls.py
+++ typeideaApp/urls.py
@@ -21,10 +21,10 @@ from blog.views import post_list, post_detail
 
 from .custom_site import custom_site
 urlpatterns = [
-    url(r'^$', post_list),
-    url(r'^category/(?P<category_id>\d+)/$', post_list),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
-    url(r'^post/(?P<post_id>\d+)/$', post_detail),
+    url(r'^$', post_list, name="index"),
+    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category'),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag'),
+    url(r'^post/(?P<post_id>\d+)/$', post_detail, name='detail'),
     # url(r'^links/$', links),
     url(r'^admin/', admin.site.urls),
     url(r'^cus_admin/', custom_site.urls),
