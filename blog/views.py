010b232fda3579267ab3424c350f1a78196a7754
superrabbit11223344 <gongwei808@hotmail.com>
2019-08-03 10:23:14 +0800

    完成分页功能

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage
 
 from .models import Post, Tag
 
 
 def post_list(request, category_id=None, tag_id=None):
     queryset = Post.objects.all()
+    page = request.GET.get('page', 1)
+    page_size = 4
+    try:
+        page = int(page)
+    except TypeError:
+        page = 1
+
     if category_id:
         # 分类页面
-        qeuryset = queryset.filter(category_id=category_id)
+        queryset = queryset.filter(category_id=category_id)
     elif tag_id:
         # 标签页面
         try:
@@ -17,8 +25,13 @@ def post_list(request, category_id=None, tag_id=None):
             queryset = []
         else:
             queryset = tag.post_set.all()
+    paginator = Paginator(queryset, page_size)
+    try:
+        posts = paginator.page(page)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     context = {
-        'posts': queryset
+        'posts': posts,
     }
     return render(request, 'blog/list.html', context=context)
 
