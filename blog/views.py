baa920e71295346dd14f0aa7fd68686b7846b6bf
superrabbit11223344 <gongwei808@hotmail.com>
2019-08-22 09:12:19 +0800

    修改

--- blog/views.py
+++ blog/views.py
@@ -1,46 +1,130 @@
+from django.db.models import Q
 from django.views.generic import View, ListView, DetailView  # ListView 更适合做列表展示
-from django.views.generic.detail import SingleObjectMixin
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
 
 from .models import Post, Tag, Category
 from config.models import SideBar
-from comment.models import Comment
 
 
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
 
-    if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_list = []
-        else:
-            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
-    else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            post_list = post_list.filter(category_id=category_id)
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/list.html', context=context)
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all()
+        })
+        context.update(Category.get_navs())
+        return context
 
 
-def post_detail(request, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+    # E:\typeideaApp\templates\themes\bootstrap\default\templates\blog\list.html
 
-    context = {
-        'post': post,
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/detail.html', context=context)
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id = tag_id)
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+
+class SearchView(IndexView):
+    def get_context_data(self):
+        context = super().get_context_data()
+        context.update({
+            'keyword': self.request.GET.get('keyword', '')
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+        if not keyword:
+            return queryset
+        return queryset.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def post_list(request, category_id=None, tag_id=None):
+#     tag = None
+#     category = None
+#
+#     if tag_id:
+#         try:
+#             tag = Tag.objects.get(id=tag_id)
+#         except Tag.DoesNotExist:
+#             post_list = []
+#         else:
+#             post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+#     else:
+#         post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+#         if category_id:
+#             post_list = post_list.filter(category_id=category_id)
+#     context = {
+#         'category': category,
+#         'tag': tag,
+#         'post_list': post_list,
+#         'sidebars': SideBar.get_all(),
+#     }
+#     context.update(Category.get_navs())
+#     return render(request, 'blog/list.html', context=context)
+#
+#
+# def post_detail(request, post_id):
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#
+#     context = {
+#         'post': post,
+#     }
+#     context.update(Category.get_navs())
+#     return render(request, 'blog/detail.html', context=context)
--- typeideaApp/urls.py
+++ typeideaApp/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.conf.urls import url
-from blog.views import post_list, post_detail
+# from blog.views import post_list, post_detail
 from config.views import links
-# from blog.views import IndexView, CategoryView, TagView, PostView
+from blog.views import IndexView, CategoryView, TagView, PostDetailView, SearchView
 
 from .custom_site import custom_site
 
 urlpatterns = [
-    url(r'^$', post_list, name="index"),
-    url(r'^category/(?P<category_id>\d+)/$', post_list, name="category-list"),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name="tag-list"),
-    url(r'^post/(?P<post_id>\d+).html/$', post_detail, name="post-detail"),
+    url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name="category-list"),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name="tag-list"),
+    url(r'^post/(?P<post_id>\d+).html/$', PostDetailView.as_view(), name="post-detail"),
     url(r'^links/$', links, name="links"),
+    url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^admin/', admin.site.urls),
     url(r'^cus_admin/', custom_site.urls),
 ]
